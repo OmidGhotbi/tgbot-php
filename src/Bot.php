cfcc3a41473cbf136168903acae89367c2e426c8
pathetic <pathetic.underscore@gmail.com>
2015-06-27 19:48:28 +0000

    Add getUpdates method.

--- src/Bot.php
+++ src/Bot.php
@@ -7,6 +7,7 @@ use Pathetic\TgBot\Exception as TgBotException;
 use Pathetic\TgBot\Types\User;
 use Pathetic\TgBot\Types\Message;
 use Pathetic\TgBot\Types\UserProfilePhotos;
+use Pathetic\TgBot\Types\Update;
 
 class Bot
 {
@@ -255,5 +256,27 @@ class Bot
     public function getUserProfilePhotos($user_id, $offset = null, $limit = null)
     {
         return new UserProfilePhotos($this->request('getUserProfilePhotos', compact('user_id', 'offset', 'limit')));
+    }
+    
+    /**
+     * Use this method to receive incoming updates using long polling (wiki). An Array of Update objects is returned.
+     * 
+     * @var integer|null $offset  Identifier of the first update to be returned. Must be greater by one than the highest among the identifiers of previously received updates. By default, updates starting with the earliest unconfirmed update are returned. An update is considered confirmed as soon as getUpdates is called with an offset higher than its update_id.
+     * @var integer|null $limit   Limits the number of updates to be retrieved. Values between 1—100 are accepted. Defaults to 100
+     * @var integer|null $timeout Timeout in seconds for long polling. Defaults to 0, i.e. usual short polling
+     * 
+     * @return array
+     */
+    public function getUpdates($offset = null, $limit = null, $timeout = null)
+    {
+        $arrayOfUnsortedUpdates = $this->request('getUpdates', compact('offset', 'limit', 'timeout'));
+        
+        $updates = [];
+        
+        foreach ($arrayOfUnsortedUpdates as $update) {
+            $updates[] = new Update($update);
+        }
+        
+        return $updates;
     }
 }
