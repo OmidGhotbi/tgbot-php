76ade386a69d576adb363b3f7b45469269ac4f20
pathetic <pathetic.underscore@gmail.com>
2015-06-29 22:04:31 +0000

    Add ContactTest

--- tests/Types/AudioTest.php
+++ tests/Types/AudioTest.php
@@ -6,15 +6,33 @@ use Pathetic\TgBot\Types\Audio;
 
 class AudioTest extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * @var \Pathetic\TgBot\Types\Audio
+     */
     protected $audio;
     
     public function setUp()
     {
-        $this->audio = new Audio(['file_id' => 1]);
+        $this->audio = new Audio(['file_id' => 'AwADAgADAgAD3xCtBRZ1GtpgRnK5Ag', 'duration' => 1, 'mime_type' => 'audio/mpeg', 'file_size' => 15516232]);
     }
     
     public function testFileId()
     {
-        $this->assertAttributeEquals(1, 'file_id', $this->audio);
+        $this->assertAttributeEquals('AwADAgADAgAD3xCtBRZ1GtpgRnK5Ag', 'file_id', $this->audio);
+    }
+    
+    public function testDuration()
+    {
+        $this->assertAttributeEquals(1, 'duration', $this->audio);
+    }
+    
+    public function testMimeType()
+    {
+        $this->assertAttributeEquals('audio/mpeg', 'mime_type', $this->audio);
+    }
+    
+    public function testFileSize()
+    {
+        $this->assertAttributeEquals(15516232, 'file_size', $this->audio);
     }
 }
